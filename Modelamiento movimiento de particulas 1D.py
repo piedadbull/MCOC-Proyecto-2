9299a65c000252ec37dbb838bb57e411842eb7f6
Pedro Naretto <37981698+PedroNaretto@users.noreply.github.com>
2019-10-04 18:26:19 -0300

    Integrador para 2 particulas

--- Modelamiento movimiento de particulas 1D.py
+++ Modelamiento movimiento de particulas 1D.py
@@ -13,13 +13,15 @@ vfx = 5.0*_m/_s #m/s
 vfy = 0.1 *_m/_s#m/s
 
 
-x0 = array([0.,1.*_mm], dtype=double)
-v0 = array([1.,1.], dtype=double)
+x01 = array([0.,1.*_mm], dtype=double)
+v01 = array([1.,1.], dtype=double)
+x02 = array([0.,1.*_mm], dtype=double)
+v02 = array([1.,2.], dtype=double)
 
-xi = x0#zeros (2, dtype= double) #posicion actual
-vi = v0#zeros (2, dtype= double)	#velocidad actual
-xim1 = zeros (2, dtype= double)	#posicion siguiente
-vim1 = zeros (2, dtype= double)	#velocidad siguiente
+#xi = x0   #zeros (2, dtype= double) #posicion actual
+#vi = v0   #zeros (2, dtype= double)	#velocidad actual
+#xim1 = zeros (2, dtype= double)	#posicion siguiente
+#vim1 = zeros (2, dtype= double)	#velocidad siguiente
 
 g = 9.81*_m/_s**2
 d = 1 * _mm
@@ -45,50 +47,64 @@ Nt = len(t)
 #Nt = int32(2*tmax/ dt) #numero de espacios de tiempo, tiene que ser entero porque hare un for
 norm = lambda v: sqrt(dot(v,v))
 
-k_penal = 1000*0.5*Cd*rho_agua*A*norm(v0)/(1*_mm)
-
-
+k_penal = 1000*0.5*Cd*rho_agua*A*norm(v01)/(1*_mm)
 
 
 def particula(z,t):
-	xi = z[:2]
-	vi = z[2:]
-	vf = array([vfx,vfy])
-	vrel = vf-vi
-	fD = (0.5*Cd*rho_agua*norm(vrel)*A)*vrel
-	Fi = W + fD + fB
+	i = 0
 
-	if xi[1]<0:
-		Fi[1] += -k_penal*xi[1]
+	zp = zeros(len(z))
+	for p in range((len(z))/4):
+		xi = z[i:2+i]
+		vi = z[2+i:4+i]
+		vf = array([vfx,vfy])
+		vrel = vf-vi
+		fD = (0.5*Cd*rho_agua*norm(vrel)*A)*vrel
+		Fi = W + fD + fB
 
-	zp = zeros(4)
-	zp[:2] = vi
+		if xi[1]<0:
+			Fi[1] += -k_penal*xi[1]
 
-	zp[2:] = Fi/m
+		zp[i:2+i] = vi
+		zp[2+i:4+i] = Fi/m
+
+		i += 4.
+
 	return zp
 
 
 from scipy.integrate import odeint
-z0 = zeros(4)
-z0[:2] = x0
-z0[2:] = v0
+n = 2   #Numero de particulas
+z0 = zeros(4*n)
+z0[:2] = x01
+z0[2:4] = v01
+z0[4:6] = x02
+z0[6:8] = v02
+
+
 z = odeint(particula, z0, t)
-x = z[:,:2]
-v = z[:,2:]
+x1 = z[:,:2]
+v1 = z[:,2:4]
+x2 = z[:,4:6]
+v2 = z[:,6:8]
+
+print (x2,v2)
 
 figure()
-plot(x[:,0],x[:,1])
-ylim([0,10*_mm])
+plot(x1[:,0],x1[:,1],label="x1")
+plot(x2[:,0],x2[:,1],label="x2")
+ylim([0,20*_mm])
+plt.legend()
 
 
 figure()
 subplot(2,1,1)
-plot(t,x[:,0],label="x")
-plot(t,x[:,1],label="y")
+plot(t,x1[:,0],label="x")
+plot(t,x1[:,1],label="y")
 plt.legend()
 subplot(2,1,2)
-plot(t,v[:,0],label="vx")
-plot(t,v[:,1],label="vy")
+plot(t,v1[:,0],label="vx")
+plot(t,v1[:,1],label="vy")
 
 show()
 
