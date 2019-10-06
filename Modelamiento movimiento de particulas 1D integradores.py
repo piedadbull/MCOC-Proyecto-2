20a923f34af1471504908f3f17776c38c1c25190
PedroNaretto <ppnaretto@miuandes.cl>
2019-10-06 20:40:37 -0300

    Arreglo error de indices (que no se pq no funcionaba)

--- Modelamiento movimiento de particulas 1D integradores.py
+++ Modelamiento movimiento de particulas 1D integradores.py
@@ -13,10 +13,10 @@ vfx = 5.0*_m/_s #m/s
 vfy = 0.1 *_m/_s#m/s
 
 
-x01 = array([0.,1.*_mm], dtype=double)
-v01 = array([1.,1.], dtype=double)
-x02 = array([0.,1.*_mm], dtype=double)
-v02 = array([1.,2.], dtype=double)
+x01 = array([[0.,1.*_mm],[0.,1.*_mm]], dtype=double)   # matriz posicion inicial [x,y] particula [1, 2, 3, ..., n]
+v01 = array([[1.,1.],[1.,2.]], dtype=double)   # matriz velocidad inicial [vx,vy] particula [1, 2, 3, ..., n]
+#x02 = array([0.,1.*_mm], dtype=double)
+#v02 = array([1.,2.], dtype=double)
 
 #xi = x0   #zeros (2, dtype= double) #posicion actual
 #vi = v0   #zeros (2, dtype= double)	#velocidad actual
@@ -45,9 +45,10 @@ fB = array([0, rho_agua*V*g])
 t = arange(0,tmax,dt)
 Nt = len(t)
 #Nt = int32(2*tmax/ dt) #numero de espacios de tiempo, tiene que ser entero porque hare un for
+
 norm = lambda v: sqrt(dot(v,v))
 
-k_penal = 1000*0.5*Cd*rho_agua*A*norm(v01)/(1*_mm)
+k_penal = lambda v: 1000*0.5*Cd*rho_agua*A*norm(v)/(1*_mm)
 
 
 def particula(z,t):
@@ -61,7 +62,7 @@ def particula(z,t):
 		Fi = W + fD + fB
 
 		if xi[1]<0:
-			Fi[1] += -k_penal*xi[1]
+			Fi[1] += -k_penal(vi)*xi[1]
 
 		zp[p*4:2+p*4] = vi
 		zp[2+p*4:4+p*4] = Fi/m
@@ -70,37 +71,52 @@ def particula(z,t):
 
 
 from scipy.integrate import odeint
-n = 2   #Numero de particulas
+n = len(x01)   #Numero de particulas
 z0 = zeros(4*n)
-z0[:2] = x01
-z0[2:4] = v01
-z0[4:6] = x02
-z0[6:8] = v02
+for p in range(n):
+    z0[p*4:2+p*4] = x01[p]
+    z0[2+p*4:4+p*4] = v01[p]
+    
+print z0
+#z0[:2] = x01
+#z0[2:4] = v01
+#z0[4:6] = x02
+#z0[6:8] = v02
 
 
 z = odeint(particula, z0, t)
-x1 = z[:,:2]
-v1 = z[:,2:4]
-x2 = z[:,4:6]
-v2 = z[:,6:8]
 
-print (x2,v2)
+
+#x1 = z[:,:2]
+#v1 = z[:,2:4]
+#x2 = z[:,4:6]
+#v2 = z[:,6:8]
 
 figure()
-plot(x1[:,0],x1[:,1],label="x1")
-plot(x2[:,0],x2[:,1],label="x2")
+for p in range(n):
+    x1 = z[:,p*4:2+p*4]
+    plot(x1[:,0],x1[:,1],label="x"+str(p+1))
+    
+#plot(x1[:,0],x1[:,1],label="x1")
+#plot(x2[:,0],x2[:,1],label="x2")
 ylim([0,20*_mm])
 plt.legend()
 
 
 figure()
-subplot(2,1,1)
-plot(t,x1[:,0],label="x")
-plot(t,x1[:,1],label="y")
-plt.legend()
-subplot(2,1,2)
-plot(t,v1[:,0],label="vx")
-plot(t,v1[:,1],label="vy")
+for p in range(n):
+    subplot(2,n,p+1)
+    x1 = z[:,p*4:2+p*4]
+    plot(t,x1[:,0],label="x"+str(p+1))
+    plot(t,x1[:,1],label="y"+str(p+1))
+    plt.legend()
+
+for p in range(n):
+    subplot(2,n,p+n+1)
+    v1 = z[:,2+p*4:4+p*4]
+    plot(t,v1[:,0],label="vx"+str(p+1))
+    plot(t,v1[:,1],label="vy"+str(p+1))
+    plt.legend()
 
 show()
 
