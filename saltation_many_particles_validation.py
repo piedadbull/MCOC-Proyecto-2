b77dc8b3c5a3eb0f7beeaec4d9976ed164965fd6
PedroNaretto <ppnaretto@miuandes.cl>
2019-10-07 21:12:37 -0500

    Arreglo de choques

--- saltation_many_particles_validation.py
+++ saltation_many_particles_validation.py
@@ -19,8 +19,8 @@ n = 4   #Numero de particulas
 x01 = zeros((n,2))   #matriz posicion de las particulas
 v01 = zeros((n,2))   #matriz velocidad de las particulas
 for i in range(n):
-	x01[i][:2] = array([0.,double(random.randint(0,10)*_mm)])                         #valores iniciales de la posicion
-	v01[i][:2] = array([double(random.randint(0,10)),double(random.randint(0,10))])   # y la velocidad de la particula
+	x01[i][:2] = array([0.,double((random.randint(0,10)+random.random())*_mm)])   #valores iniciales de la posicion
+	v01[i][:2] = array([double(random.randint(0,10)+random.random()),double(random.randint(0,10)+random.random())])   # y la velocidad de la particula
 
 print("Condiciones iniciales:")
 print("Posiociones =",x01)
@@ -49,8 +49,8 @@ m = rho_particula*V   #masa de la particula, grano de arena
 
 
 
-dt = 0.001*_s #paso de tiempo
-tmax = 2 *_s#tiempo maximo de simulacion
+dt = 0.0001*_s #paso de tiempo
+tmax = 3 *_s#tiempo maximo de simulacion
 ti = 0.*_s#tiempo actual
 
 W = array([0, -m*g])   #Vector peso de la particula
@@ -60,17 +60,16 @@ t = arange(0,tmax,dt)
 Nt = len(t)
 #Nt = int32(2*tmax/ dt) #numero de espacios de tiempo, tiene que ser entero porque hare un for
 
-norm = lambda v: sqrt(dot(v,v))
+norm = lambda v: sqrt(dot(v,v))   #Funcion norma
 
-k_penal = lambda v: 1000*0.5*Cd*rho_agua*A*norm(v)/(1*_mm)
+k_penal = 1000*0.5*Cd*rho_agua*A*norm(vfx)/(1*_mm)   #Constante de resorte
 
-vfx = lambda xy: 1./0.4*log(30*(xy+1./30))
-choque = lambda x1,x2: sqrt((x1[0]-x2[0])**2+(x1[1]-x2[1])**2)   #Distancia entre 2 particulas
+vfx = lambda xy: 1./0.4*log(30*(xy+1./30.))
+dist = lambda x1,x2: sqrt((x1[0]-x2[0])**2+(x1[1]-x2[1])**2)   #Distancia entre 2 particulas
+
 def angulo(x1,x2):   #Angulo de choque entre 2 particulas
-	if abs(x1[0]-x2[0]) > 0:
-		return arctan(abs(x1[1]-x2[1])/abs(x1[0]-x2[0])) 
-	else:
-		return 0.
+	return arctan(abs(x1[1]-x2[1])/abs(x1[0]-x2[0])) 
+
 
 
 def particula(z,t):
@@ -84,20 +83,20 @@ def particula(z,t):
 		fD = (0.5*Cd*rho_agua*norm(vrel)*A)*vrel
 		Fi = W + fD + fB
 
-		if xi[1]<0:
-			Fi[1] += -k_penal(vi)*xi[1]
+		if xi[1] < d/2.:   #Cuando centro de la particula se encuentra a una distancia de d/2 se produce el choque contra el piso
+			Fi[1] += -k_penal*(xi[1]-d/2)
 
 		for p2 in range((len(z))/4):   #Revisamos si hay choque entre la particula p1 y alguna otra particula
 			if p1 != p2:
 				x2 = z[p2*4:2+p2*4]    #Posicion y velocidad de la 2da particula
 				v2 = z[2+p2*4:4+p2*4]
-				xdif = choque(xi,x2)   #Distancia entre los centros de las particulas
-				if xdif < 1 * _mm:     #Revisamos si hay choque revisando si la diferencia de posiciones es menor a 1 diametro de particula
+				xdif = dist(xi,x2)   #Distancia entre los centros de las particulas
+				if xdif < d:         #Revisamos si hay choque revisando si la diferencia de posiciones es menor a 1 diametro
 					vdif = vi-v2
-					Fct = abs(-k_penal(vdif)*xdif)   #La misma idea de choque contra el suelo
-					theta = angulo(xi,x2)            #Angulo con que chocan las particulas
-					if xi[0] > x2[0]:                #Revisamos posicion de la particula con respecto a la que choco para ver si
-						Fcx = Fct*cos(theta)         # acelera o desacelera en el eje x e y
+					Fct = abs(-k_penal*(xdif-d))   #La misma idea de choque contra el suelo
+					theta = angulo(xi,x2)          #Angulo con que chocan las particulas
+					if xi[0] > x2[0]:              #Revisamos posicion de la particula con respecto a la que choco para ver si
+						Fcx = Fct*cos(theta)       # acelera o desacelera en el eje x e y
 					else:
 						Fcx = -Fct*cos(theta)
 					if xi[1] >= x2[1]:
@@ -106,7 +105,7 @@ def particula(z,t):
 						Fcy = -Fct*cos(theta)
 					Fc = [Fcx, Fcy]
 					Fi += Fc
-					print ("choque")
+					print (t,"choque")
 
 		zp[p1*4:2+p1*4] = vi      #Guardamos la derivada de la posicion con respecto al tiempo
 		zp[2+p1*4:4+p1*4] = Fi/m  #Guardamos la derivada de la velocidad con respecto al tiempo
@@ -116,7 +115,7 @@ def particula(z,t):
 
 from scipy.integrate import odeint
 
-z0 = zeros(4*n)      #Vector de condiciones iniciales para la integracion, guardamos de la forma
+z0 = zeros(4*n)      #Vector de condiciones iniciales para la integracion. Guardamos de la forma
 for p in range(n):   # z0 = [x1,y1,vx1,vy1,x2,y2,vx2,vy2,x3,y3,vx3,vy3,....,xn,yn,vxn,vyn]
     z0[p*4:2+p*4] = x01[p]
     z0[2+p*4:4+p*4] = v01[p]
